1aebd50ed4db98156e3fd8c05e4450be892c279f
retro0 <kolya.tcherstvoff@yandex.ru>
2020-04-10 21:24:41 +0400

    Мой первый скрипт

---
+++ main.js
@@ -0,0 +1,22 @@
+'use strict';
+let money = prompt("Ваш доход?");
+let timenow = prompt("Введите дату в формате YYYY-MM-DD");
+
+let expen = prompt("Введите обязательную статью расходов в этом месяце");
+let howmuch = prompt("Во сколько обойдется?");
+
+let expenses = { 
+    p1: expen,
+    p2: howmuch,
+};
+let income = [];
+
+let appData = {
+    budjet: money,  //бюджет (передаем сюда переменную из п.2)
+    time:  timenow, // данные времени - timeData (передаем сюда переменную из п.2)
+    ex: expenses,
+    optionalExpenses: "",
+    savings: false
+
+};
+alert((expenses.p1 * expenses.p2) - money);
